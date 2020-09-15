db284eb1d2977b89883e65574f1faef1879961d7
Rae Farine <rfarine1027@gmail.com>
2020-09-15 16:35:38 -0400

    Successfully authorize Github token

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
-  "context"
   "os"
+  "fmt"
+  "context"
   "github.com/sethvargo/go-githubactions"
   "golang.org/x/oauth2"
   "github.com/shurcooL/githubv4"
 )
 
 func main() {
+  // Check for all required inputs
   ghToken := githubactions.GetInput("ghToken")
-
-  githubactions.Debugf("ghToken is...%d", ghToken)
 
   if ghToken == "" {
     githubactions.Fatalf("missing input 'ghToken'")
   }
 
-  githubactions.AddMask(ghToken)
-
   // Github Auth
+  fmt.Println("Authorizing Github API access...")
+
   src := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: os.Getenv("ghToken")},
+    &oauth2.Token{AccessToken: os.Getenv("INPUT_GHTOKEN")},
   )
 
   httpClient := oauth2.NewClient(context.Background(), src)
-
   client := githubv4.NewClient(httpClient)
 
+  var query struct {
+    Viewer struct {
+      Login     githubv4.String
+      CreatedAt githubv4.DateTime
+    }
+  }
+
+  fmt.Println("Querying Github API...")
+
+  err := client.Query(context.Background(), &query, nil)
+
+  if err != nil {
+    githubactions.Fatalf("Error querying Github: %s", err)
+  }
+
+  githubactions.Debugf("Query results: %s", query.Viewer.Login)
 }
